46a484e98b9045ae0cf410ac84d87189e26adf5b
Marcin Dulak <Marcin.Dulak@gmail.com>
2020-12-27 23:37:14 +0100

    Add dataset register module and tests

---
+++ nd00333_capstone/dataset/register/register.py
@@ -0,0 +1,145 @@
+import argparse
+import glob
+import os
+import pathlib
+
+from azureml.core import Workspace
+from azureml.data import DataType
+from azureml.data.datapath import DataPath
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+from nd00333_capstone.dataset.load import load
+
+import logging
+from pythonjsonlogger import jsonlogger
+
+logger = logging.getLogger()
+
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+logger.setLevel(logging.INFO)
+
+
+VERSION = "1"
+
+DTYPE_MAP = {"int64": DataType.to_long(), "object": DataType.to_string()}
+DATA_TYPES = {feature: DTYPE_MAP[dtype] for feature, dtype in load.DTYPE.items()}
+DEFAULT_ARGS = {
+    "--dataset-path": "datasets",
+    "--dataset-name": "ids2018",
+    "--dataset-version": VERSION,
+    "--dataset-overwrite": False,
+    "--dry-run": False,
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset-path",
+        required=False,
+        type=str,
+        default=DEFAULT_ARGS["--dataset-path"],
+        help="Root path of the local datasets",
+    )
+    parser.add_argument(
+        "--dataset-name",
+        required=True,
+        type=str,
+        default=DEFAULT_ARGS["--dataset-name"],
+        help="Name of the input dataset",
+    )
+    parser.add_argument(
+        "--dataset-version",
+        required=False,
+        type=str,
+        default=DEFAULT_ARGS["--dataset-version"],
+        help="Version of the dataset",
+    )
+    parser.add_argument(
+        "--dataset-overwrite",
+        action="store_true",
+        default=DEFAULT_ARGS["--dataset-overwrite"],
+        help="Whether to overwrite the remote dataset",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=DEFAULT_ARGS["--dry-run"],
+        help="Dry run",
+    )
+    return parser.parse_args()
+
+
+def get_workspace():
+    workspace = Workspace.from_config()
+    return workspace
+
+
+def get_default_datastore(workspace):
+    datastore = workspace.get_default_datastore()
+    return datastore
+
+
+def upload_files(datastore, **kwargs):
+    return datastore.upload_files(**kwargs)
+
+
+def datastore_upload_files(args):
+    ws = get_workspace()
+    datastore = get_default_datastore(ws)
+
+    directory = pathlib.Path(args.dataset_path, args.dataset_name)
+    if not os.path.exists(directory):
+        msg = f"The dataset directory {directory} does not exist"
+        logger.exception(msg)
+        raise RuntimeError(msg)
+
+    files = [os.path.abspath(file) for file in sorted(glob.glob(f"{directory}/*.csv"))]
+    target_path = f"{args.dataset_name}_{args.dataset_version}"
+    kwargs = {
+        "files": files,
+        "target_path": target_path,
+        "overwrite": args.dataset_overwrite,
+    }
+    logger.info(msg="datastore.upload_files", extra={"kwargs": kwargs})
+    if not args.dry_run:
+        try:
+            _ = upload_files(datastore, **kwargs)
+        except:
+            msg = f"Upload to target_path {target_path} failed"
+            logger.exception(msg)
+            raise RuntimeError(msg)
+
+    datastore_path = [
+        DataPath(datastore, str(pathlib.Path(target_path, os.path.basename(file))))
+        for file in files
+    ]
+
+    return datastore_path, target_path
+
+
+def dataset_register(args):
+    ws = get_workspace()
+    datastore = get_default_datastore(ws)
+
+    datastore_path, target_path = datastore_upload_files(args)
+
+    kwargs = {"path": datastore_path, "set_column_types": DATA_TYPES}
+    logger.info(
+        msg="TabularDatasetFactory.from_delimited_files", extra={"kwargs": kwargs}
+    )
+    if not args.dry_run:
+        tabular = TabularDatasetFactory.from_delimited_files(**kwargs)
+
+    kwargs = {"workspace": ws, "name": target_path, "create_new_version": False}
+    logger.info(msg="tabular.register", extra={"kwargs": kwargs})
+    if not args.dry_run:
+        dataset = tabular.register(**kwargs)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    _ = dataset_register(args)
---
+++ tests/dataset/register/test_register.py
@@ -0,0 +1,74 @@
+import collections
+import pathlib
+import shutil
+import inspect
+import numpy as np
+import pandas as pd
+import pandas.testing
+import pytest
+
+from nd00333_capstone.dataset.register import register
+from tests import utils
+
+
+mock_datastore = collections.namedtuple("datastore", "name")
+
+ARGS = sorted(register.DEFAULT_ARGS.keys())
+mock_args = collections.namedtuple(
+    "args",
+    (arg[2:].replace("-", "_") for arg in ARGS),
+    defaults=(register.DEFAULT_ARGS[arg] for arg in ARGS),
+)
+
+
+@pytest.fixture
+def mocker(mocker):
+    mocker.patch(
+        "nd00333_capstone.dataset.register.register.get_workspace",
+        return_value="mocker",
+    )
+    mocker.patch(
+        "nd00333_capstone.dataset.register.register.get_default_datastore",
+        return_value=mock_datastore(name="mocker"),
+    )
+    return mocker
+
+
+def test_datastore_upload_files_directory_does_not_exist(mocker):
+    dataset_path = "mocker"
+    dataset_name = "mocker"
+    mocker.patch(
+        "nd00333_capstone.dataset.register.register.parse_args",
+        return_value=mock_args(dataset_path=dataset_path, dataset_name=dataset_name),
+    )
+    args = register.parse_args()
+    directory = pathlib.Path(args.dataset_path, args.dataset_name)
+    with pytest.raises(RuntimeError) as excinfo:
+        register.datastore_upload_files(register.parse_args())
+    assert str(excinfo.value).startswith(
+        f"The dataset directory {directory} does not exist"
+    )
+
+
+def test_datastore_upload_files_overwrite_false_succeed(mocker):
+    directory = utils.get_directory("dataset", inspect.currentframe().f_code.co_name)
+    dataset_path = directory.parent
+    dataset_name = directory.name
+    mocker.patch(
+        "nd00333_capstone.dataset.register.register.parse_args",
+        return_value=mock_args(
+            dataset_path=dataset_path, dataset_name=dataset_name, dataset_version="2"
+        ),
+    )
+    mocker.patch(
+        "nd00333_capstone.dataset.register.register.upload_files",
+        return_value="",
+    )
+    filename = pathlib.Path(directory, "1.csv")
+    data = [["Benign"]]
+    columns = ["Label"]
+    _ = pd.DataFrame(data=data, columns=columns).to_csv(filename, index=False)
+    args = register.parse_args()
+    datastore_path, target_path = register.datastore_upload_files(register.parse_args())
+    assert len(datastore_path) == 1
+    assert target_path == f"{args.dataset_name}_{args.dataset_version}"
