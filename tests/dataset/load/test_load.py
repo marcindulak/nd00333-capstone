5c3d36b98fc7c6d83426e9f548ed523b9af383d9
Marcin Dulak <Marcin.Dulak@gmail.com>
2020-12-28 20:43:46 +0100

    Test fractions in train, test and validate datasets

--- tests/dataset/load/test_load.py
+++ tests/dataset/load/test_load.py
@@ -9,9 +9,28 @@ import pytest
 from nd00333_capstone.dataset.load import load
 from tests import utils as tests_utils
 
+LABEL_FRACTIONS = {
+    "Benign": 0.842494,
+    "Bot": 0.017646,
+    "Brute Force -Web": 3.3e-05,
+    "Brute Force -XSS": 1.4e-05,
+    "DDOS attack-HOIC": 0.020204,
+    "DDoS attacks-LOIC-HTTP": 0.035694,
+    "DoS attacks-GoldenEye": 0.003427,
+    "DoS attacks-Hulk": 0.003185,
+    "DoS attacks-SlowHTTPTest": 0.01726,
+    "DoS attacks-Slowloris": 0.001065,
+    "FTP-BruteForce": 0.023857,
+    "Infilteration": 0.01197,
+    "SQL Injection": 7e-06,
+    "SSH-Bruteforce": 0.023145,
+}
+
 
 def test_get_df_from_csv_default():
-    directory = tests_utils.get_directory("dataset", inspect.currentframe().f_code.co_name)
+    directory = tests_utils.get_directory(
+        "dataset", inspect.currentframe().f_code.co_name
+    )
     filename = pathlib.Path(directory, "1.csv")
     data = [["Benign"]]
     columns = ["Label"]
@@ -23,7 +42,9 @@ def test_get_df_from_csv_default():
 
 
 def test_get_df_from_csv_dtype_int():
-    directory = tests_utils.get_directory("dataset", inspect.currentframe().f_code.co_name)
+    directory = tests_utils.get_directory(
+        "dataset", inspect.currentframe().f_code.co_name
+    )
     filename = pathlib.Path(directory, "1.csv")
     data = ["123", "456"]
     dtype = [("Label", np.dtype(int))]
@@ -36,7 +57,9 @@ def test_get_df_from_csv_dtype_int():
 
 
 def test_get_df_from_csv_usecols():
-    directory = tests_utils.get_directory("dataset", inspect.currentframe().f_code.co_name)
+    directory = tests_utils.get_directory(
+        "dataset", inspect.currentframe().f_code.co_name
+    )
     filename = pathlib.Path(directory, "1.csv")
     data = [("1", "Benign"), ("2", "Malicious")]
     dtype = [("dummy1", np.dtype(int)), ("Label", np.dtype(object))]
@@ -50,7 +73,9 @@ def test_get_df_from_csv_usecols():
 
 
 def test_get_df_from_directory_default():
-    directory = tests_utils.get_directory("dataset", inspect.currentframe().f_code.co_name)
+    directory = tests_utils.get_directory(
+        "dataset", inspect.currentframe().f_code.co_name
+    )
     columns = ["Label"]
     for iter in [0, 1, 2]:
         _ = pd.DataFrame(data=[iter], columns=columns).to_csv(
@@ -108,33 +133,79 @@ def test_get_df_from_directory_dataset_full_fractions(dataset):
         .groupby("Label")
         .agg(count=("Flow Duration", "count"))
     )
-    fractions = {
-        "Benign": 0.842494,
-        "Bot": 0.017646,
-        "Brute Force -Web": 3.3e-05,
-        "Brute Force -XSS": 1.4e-05,
-        "DDOS attack-HOIC": 0.020204,
-        "DDoS attacks-LOIC-HTTP": 0.035694,
-        "DoS attacks-GoldenEye": 0.003427,
-        "DoS attacks-Hulk": 0.003185,
-        "DoS attacks-SlowHTTPTest": 0.01726,
-        "DoS attacks-Slowloris": 0.001065,
-        "FTP-BruteForce": 0.023857,
-        "Infilteration": 0.01197,
-        "SQL Injection": 7e-06,
-        "SSH-Bruteforce": 0.023145,
-    }
     reference = (
-        pd.DataFrame.from_dict(fractions, orient="index")
+        pd.DataFrame.from_dict(LABEL_FRACTIONS, orient="index")
         .reset_index()
         .rename(columns={"index": "Label", 0: "count"})
     ).set_index("Label")
     result = (
         (grouped["count"] / grouped["count"].sum()).reset_index().set_index("Label")
     )
-    for label in sorted(fractions.keys()):
+    for label in sorted(LABEL_FRACTIONS.keys()):
         assert np.isclose(result.loc[label], reference.loc[label], rtol=0.1, atol=0)
 
 
-def test_get_df_from_directory_dataset_test_columns(dataset):
-    assert list(sorted(dataset("ids2018test").columns)) == sorted(load.DTYPE.keys())
+def test_get_df_from_directory_dataset_train_fractions(dataset):
+    grouped = (
+        dataset("ids2018train")[["Flow Duration", "Label"]]
+        .groupby("Label")
+        .agg(count=("Flow Duration", "count"))
+    )
+    reference = (
+        pd.DataFrame.from_dict(LABEL_FRACTIONS, orient="index")
+        .reset_index()
+        .rename(columns={"index": "Label", 0: "count"})
+    ).set_index("Label")
+    result = (
+        (grouped["count"] / grouped["count"].sum()).reset_index().set_index("Label")
+    )
+    for label in sorted(LABEL_FRACTIONS.keys()):
+        if label in ["Brute Force -XSS", "SQL Injection"]:
+            rtol = 0.3
+        else:
+            rtol = 0.1
+        assert np.isclose(result.loc[label], reference.loc[label], rtol=rtol, atol=0)
+
+
+def test_get_df_from_directory_dataset_test_fractions(dataset):
+    grouped = (
+        dataset("ids2018test")[["Flow Duration", "Label"]]
+        .groupby("Label")
+        .agg(count=("Flow Duration", "count"))
+    )
+    reference = (
+        pd.DataFrame.from_dict(LABEL_FRACTIONS, orient="index")
+        .reset_index()
+        .rename(columns={"index": "Label", 0: "count"})
+    ).set_index("Label")
+    result = (
+        (grouped["count"] / grouped["count"].sum()).reset_index().set_index("Label")
+    )
+    for label in sorted(LABEL_FRACTIONS.keys()):
+        if label in ["Brute Force -XSS", "SQL Injection"]:
+            rtol = 0.3
+        else:
+            rtol = 0.1
+        assert np.isclose(result.loc[label], reference.loc[label], rtol=rtol, atol=0)
+
+
+def test_get_df_from_directory_dataset_validate_fractions(dataset):
+    grouped = (
+        dataset("ids2018validate")[["Flow Duration", "Label"]]
+        .groupby("Label")
+        .agg(count=("Flow Duration", "count"))
+    )
+    reference = (
+        pd.DataFrame.from_dict(LABEL_FRACTIONS, orient="index")
+        .reset_index()
+        .rename(columns={"index": "Label", 0: "count"})
+    ).set_index("Label")
+    result = (
+        (grouped["count"] / grouped["count"].sum()).reset_index().set_index("Label")
+    )
+    for label in sorted(LABEL_FRACTIONS.keys()):
+        if label in ["Brute Force -XSS", "SQL Injection"]:
+            rtol = 0.3
+        else:
+            rtol = 0.1
+        assert np.isclose(result.loc[label], reference.loc[label], rtol=rtol, atol=0)
