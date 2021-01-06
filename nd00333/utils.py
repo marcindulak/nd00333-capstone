0d45242d102fd407558da41dbc55c1708c8c3aed
Marcin Dulak <Marcin.Dulak@gmail.com>
2021-01-06 19:54:58 +0100

    Remove get_X_y from utils

--- nd00333/utils.py
+++ nd00333/utils.py
@@ -19,13 +19,6 @@ def get_logger():
     return logger
 
 
-def get_X_y(df, target_label):
-    """
-    Split the DataFrame into X and y using target_label as the y label
-    """
-    return df.drop(labels=[target_label], axis=1), df[target_label]
-
-
 def get_workspace():
     """
     Get the AzureML workspace from an existing config.json
