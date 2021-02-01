bc8dcd469cc7b52c140a634445b75bf44c385f37
Marcin Dulak <Marcin.Dulak@gmail.com>
2021-02-01 20:27:11 +0100

    Upload 2018 datasets script

--- scripts/datasets-register.sh
+++ scripts/datasets-register.sh
@@ -1,10 +1,12 @@
-dataset_train_name="train"
-dataset_validate_name="validate"
-dataset_test_name="test"
+dataset_name="ids2018"
+dataset_version="1"
+dataset_train_name="${dataset_name}train"
+dataset_validate_name="${dataset_name}validate"
+dataset_test_name="${dataset_name}test"
 
-az ml datastore upload --name workspaceblobstore --verbose --src-path datasets/$dataset_train_name --target-path $dataset_train_name
-az ml datastore upload --name workspaceblobstore --verbose --src-path datasets/$dataset_validate_name --target-path $dataset_validate_name
-az ml datastore upload --name workspaceblobstore --verbose --src-path datasets/$dataset_test_name --target-path $dataset_test_name
-PYTHONPATH=. python nd00333/dataset/register/register.py --dataset-path datasets --dataset-name $dataset_train_name --dataset-version 1 --dataset-type file
-PYTHONPATH=. python nd00333/dataset/register/register.py --dataset-path datasets --dataset-name $$dataset_validate_name --dataset-version 1 --dataset-type file
-PYTHONPATH=. python nd00333/dataset/register/register.py --dataset-path datasets --dataset-name $dataset_test_name --dataset-version 1 --dataset-type tabular
+time az ml datastore upload --name workspaceblobstore --verbose --src-path datasets/$dataset_train_name --target-path $dataset_train_name_${dataset_version}
+time az ml datastore upload --name workspaceblobstore --verbose --src-path datasets/$dataset_validate_name --target-path $dataset_validate_name_${dataset_version}
+time az ml datastore upload --name workspaceblobstore --verbose --src-path datasets/$dataset_test_name --target-path $dataset_test_name_${dataset_version}
+PYTHONPATH=. time python nd00333/dataset/register/register.py --dataset-path datasets --dataset-name $dataset_train_name --dataset-version 1 --dataset-type file
+PYTHONPATH=. time python nd00333/dataset/register/register.py --dataset-path datasets --dataset-name $$dataset_validate_name --dataset-version 1 --dataset-type file
+PYTHONPATH=. time python nd00333/dataset/register/register.py --dataset-path datasets --dataset-name $dataset_test_name --dataset-version 1 --dataset-type tabular
